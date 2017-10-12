5a7e60ca85338890ecc094902bf885cc12a6b82a
Jens W. Klein <jk@kleinundpartner.at>
2017-10-12 19:19:21 +0200

    add plone.app.mosaic

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
             # plone_coredev tests as of 2016-04-01.
             'plone.testing>=5.0.0',
             'plone.app.contenttypes',
+            'plone.app.mosaic',
         ],
     },
     entry_points="""
